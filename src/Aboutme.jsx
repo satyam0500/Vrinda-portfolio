f6f932ba4b86f6d47e980a34902dc16a6ede982d
KUMAR SATYAM <kumarsatyam_sh@srmuniv.edu.in>
2024-03-01 11:26:43 +0530

    footer

--- src/Aboutme.jsx
+++ src/Aboutme.jsx
@@ -4,8 +4,11 @@ import ProfilePhoto from "./assets/profilephoto.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./aboutme.css";
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import Behands from "./assets/Social.png"
+import Dribble from "./assets/Dribble.png"
+import layer1 from "./assets/Layer_1.png"
 
-const hola = ["H", "☻", "l","a"];
+const hola = ["H", "☻", "l", "a"];
 function Aboutme() {
   return (
     <>
@@ -13,17 +16,17 @@ function Aboutme() {
       <div className="aboutme-container">
         <div className="row">
           <div className="text-content">
-          <h1 className="heading-hola">
-            {hola.map((letter, index) => (
-              <span
-                className="drop-letter"
-                style={{ animationDelay: `${index * 0.5}s` }}
-                key={index}
-              >
-                {letter}
-              </span>
-            ))}
-          </h1>
+            <h1 className="heading-hola">
+              {hola.map((letter, index) => (
+                <span
+                  className="drop-letter"
+                  style={{ animationDelay: `${index * 0.5}s` }}
+                  key={index}
+                >
+                  {letter}
+                </span>
+              ))}
+            </h1>
             <p className="texts">My name is Vrinda. I'm an Interaction Design student </p>
             <p className="texts">
               My passion for design emerged as the world turned digital. I began
@@ -39,33 +42,38 @@ function Aboutme() {
           </div>
         </div>
         <div className="content">
-        <div className="hobbies">
-        <img src={ProfilePhoto} alt="Profile Photo" className="profile-photo" />
-          <h2 className="designing">Besides designing you can find me ⬇️</h2>
-          <div>
-            <p className="find-me">👋 Writing Notes To Myself</p>
-            <p className="find-me">☕ Having Coffee Hours</p>
-            <p className="find-me">🍄 Cooking Food</p>
-            <p className="find-me">🌈 Watching Anime</p>
-            <p className="find-me">🎨 Drawing in my Sketchbook</p>
-            <p className="find-me">🪴 Probably Collecting Way Too Many Notebooks</p>
-            <p className="find-me">✨ Eating Ramen </p>
+          <div className="hobbies">
+            <img src={ProfilePhoto} alt="Profile Photo" className="profile-photo" />
+            <h2 className="designing">Besides designing you can find me ⬇️</h2>
+            <div>
+              <p className="find-me">👋 Writing Notes To Myself</p>
+              <p className="find-me">☕ Having Coffee Hours</p>
+              <p className="find-me">🍄 Cooking Food</p>
+              <p className="find-me">🌈 Watching Anime</p>
+              <p className="find-me">🎨 Drawing in my Sketchbook</p>
+              <p className="find-me">🪴 Probably Collecting Way Too Many Notebooks</p>
+              <p className="find-me">✨ Eating Ramen </p>
+            </div>
           </div>
-        </div>
-        <div className="contact">
-          <h2 className="know-me">Wanna know me in person</h2>
-          <div>
-            <p className="find-me"><FontAwesomeIcon icon={faEnvelope} /> Email - </p>
-            <p className="find-me">Linkedin - </p>
-            <p className="find-me">Mobile No -</p>
+          <div className="contact">
+            <h2 className="know-me">Wanna know me in person</h2>
+            <div className="details">
+              <p className="find-me"><FontAwesomeIcon icon={faEnvelope} /> Email - </p>
+              <p className="find-me">Linkedin - </p>
+              <p className="find-me">Mobile No -</p>
+            </div>
           </div>
-        </div>
-        <div className="note">
-          <h2 className="footer">Made with lots of love and caffeine</h2>
-          <div className="images"></div>
-        </div>
-            
+          <div className="note">
+            <h2 className="footer">Made with lots of love and caffeine</h2>
+            <div className="images">
+              <a href="https://www.behance.net/vrindakhandel1">  <img src={Behands} className="behands" /></a>
+              <a href="https://dribbble.com/vrindavk
+"> <img src={Dribble} className="dribble" /></a>
+              <img src={layer1} className="layer1" />
+            </div>
           </div>
+
+        </div>
         {/* <div className="hobbies">
           <h2>Besides designing you can find me ⬇️</h2>
           <ul>
